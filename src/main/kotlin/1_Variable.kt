8e6c7b9e196969ee2e2570122d813b8847929616
sssun <98_0509@naver.com>
2023-11-24 14:34:34 +0900

    first commit

---
+++ src/main/kotlin/1_Variable.kt
@@ -0,0 +1,24 @@
+var x = 5 //탑-레벨에 위치한 변수
+
+fun main() {
+
+    x+=1
+    println(x)
+
+    val a : Int =1
+    val b = 1
+    val c : Int
+    c = 3
+
+    val d : Int = 123
+
+    //val(value)
+    //var(variable)
+
+    var e : String = "Hello"
+    println(e)
+
+    var f = 123
+    //f = "hi"
+
+}
---
+++ src/main/kotlin/3_If.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    //if..else 사용
+    val job = "Software"
+
+    //코틀린의 if...else는 표현식이다
+
+
+    //코틀린은 삼항 연산자가 없다. if..else가 표현식이므로 불필요하다
+}
