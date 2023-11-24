0d4bb30485e0a636471519a118555577d6dc932f
sssun <98_0509@naver.com>
2023-11-24 15:18:06 +0900

    while

--- src/main/kotlin/3_If.kt
+++ src/main/kotlin/3_If.kt
@@ -19,4 +19,10 @@ fun main() {
     }
 
     //코틀린은 삼항 연산자가 없다. if..else가 표현식이므로 불필요하다
+    val a = 1
+    val b = 2
+    val c = if (b>a) b else a
+
+    println(c)
+
 }
---
+++ src/main/kotlin/3_When.kt
@@ -0,0 +1,37 @@
+fun main(){
+
+    // 자바 코드를 코틀린의 when 식으로 변환한 코드
+    val day = 2
+
+    var result = when (day) {
+        1 -> "월요일"
+        2 -> "화요일"
+        3 -> "수요일"
+        4 -> "목요일"
+        5 -> "금요일"
+        else -> "주말"
+    }
+    println(result)
+
+    // else를 생략할 수 있다
+    when(getColor()) {
+        Color.RED -> println("red")
+        Color.GREEN -> println("green")
+        //else -> println("blue")
+    }
+
+
+    // 여러개의 조건을 콤마로 구분해 한줄에서 정희할 수 있다
+    when (getNumber()) {
+        0, 1 -> println("0 또는 1")
+        else -> println("0 또는 1이 아님")
+    }
+}
+
+enum class Color {
+    RED, GREEN,//BLUE
+}
+
+fun getColor() = Color.RED
+
+fun getNumber() = 2
