6a9ca07bc906c39f49379b830f01ffe877fa4ae2
sssun <98_0509@naver.com>
2023-11-24 18:31:57 +0900

    ClassProperty

---
+++ src/main/kotlin/7_ClassProperty.kt
@@ -0,0 +1,33 @@
+
+class Coffee(
+    var name: String = "아이스 커피",
+    var price : Int = 200,
+    var iced : Boolean = false,
+    ) {
+
+    //커스텀 getter setter 만들어보기
+    val brand : String
+        get() = "스타벅스"
+
+    var quantity : Int = 3
+        set(value) {
+            if (value>0) { // 수량이 0 이상인 경우에만 할당
+                field = value
+            }
+        }
+}
+
+class EmptyClass
+
+fun main() {
+
+    val coffee = Coffee()
+    coffee.name = "아이스 아메리카노"
+    coffee.price = 2000
+    coffee.quantity = 1
+    coffee.iced = true
+
+    if (coffee.iced) println("아이스 커피")
+
+    println("${coffee.name}의 가격은 ${coffee.price}원 입니다. 브랜드는 ${coffee.brand} 입니다. 남은 수량은 ${coffee.quantity}잔 입니다.")
+}
---
+++ src/main/kotlin/Java_Coffee.java
@@ -0,0 +1,26 @@
+public class Java_Coffee {
+
+    private boolean isIced;
+
+    public boolean isIced() {
+        return isIced;
+    }
+
+    public void setIced(boolean iced) {
+        isIced = iced;
+    }
+}
+
+class Barista {
+
+    public static void main(String[] args) {
+        Java_Coffee coffee = new Java_Coffee();
+        coffee.setIced(true);
+
+        if(coffee.isIced()) { // 상태를 메서드로 표현
+            System.out.println("아이스 커피");
+        } else {
+            System.out.println("따뜻한 커피");
+        }
+    }
+}
