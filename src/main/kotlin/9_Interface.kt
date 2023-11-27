4b92f799ed8fb6f7a527da2082847bff642cfaa0
sssun <98_0509@naver.com>
2023-11-28 00:14:43 +0900

    Interface

--- src/main/kotlin/9_Interface.kt
+++ src/main/kotlin/9_Interface.kt
@@ -1,27 +1,57 @@
 
 class Product(val name:String, val price: Int)
 
-interface Cart {
+interface Wheel {
+    fun roll()
+}
+
+interface Cart : Wheel {
 
     var coin: Int
 
     val weight : String
         get() = "20KG"
 
-    fun add(product: Product)
+    fun add(product: Product) {
+//        println("Cart 인터페이스의 add 함수.")
+    }
 
     fun rent() {
         if (coin > 0) {
             println("카트를 대여합니다.")
         }
     }
+
+    override fun roll() {
+        println("카트가 굴러갑니다")
+    }
+
+    fun printId() = println("1234")
+
 }
 
-class MyCart(override var coin: Int) : Cart {
+interface Order {
+    fun add(product : Product) {
+        println("${product.name} 주문이 완료 되었습니다. 가격은 ${product.price}원 입니다.")
+    }
+
+    fun printId() = println("5678")
+}
+
+class MyCart(override var coin: Int) : Cart, Order {
 
     override fun add(product: Product) {
         if (coin <= 0) println("코인을 넣어주세요.")
         else println("${product.name}이(가) 카트에 추가됐습니다.")
+
+        // 주문하기
+//        super<Cart>.add(product)
+        super<Order>.add(product)
+    }
+
+    override fun printId() {
+        super<Cart>.printId()
+        super<Order>.printId()
     }
 
 }
@@ -29,5 +59,7 @@ fun main() {
 
     var cart = MyCart(coin=100)
     cart.rent()
+    cart.roll()
     cart.add(Product(name="장난감", price=1000))
+    cart.printId()
 }
