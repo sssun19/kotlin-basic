088fb034cb3c3f43d49e8be61f00a14b159d2aaf
sssun <98_0509@naver.com>
2023-12-05 02:48:26 +0900

    DataClass

--- src/main/kotlin/kotlin_advanced/2_DataClass.kt
+++ src/main/kotlin/kotlin_advanced/2_DataClass.kt
@@ -1,6 +1,24 @@
 package kotlin_advanced
 
-data class Person (val name:String, val age: Int) {
+class Person (val name:String, val age: Int) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Person
+
+        if (name != other.name) return false
+        return age == other.age
+    }
+
+//    override fun hashCode(): Int {
+//        var result = name.hashCode()
+//        result = 31 * result + age
+//        return result
+//    }
+
+
 
 }
 
