36ec3c97c656f95da67c23e2fa360611dffb85e0
sssun <98_0509@naver.com>
2023-12-09 19:20:43 +0900

    Singleton

---
+++ src/main/kotlin/kotlin_advanced/3_Singleton.kt
@@ -0,0 +1,63 @@
+package kotlin_advanced
+
+import java.time.LocalDateTime
+import java.util.Date
+
+//
+//object Singleton {
+//
+//    val a : Int = 1234
+//
+//    fun printA() = println(a)
+//
+//}
+//
+//fun main() {
+//    println(Singleton.a)
+//    Singleton.printA()
+//}
+
+//object DatetimeUtils {
+//
+//    val now: LocalDateTime
+//        get() = LocalDateTime.now()
+//
+//    const val DEFAULT_FORMAT = "YYYY-MM-DD"
+//
+//    fun same(a: LocalDateTime, b: LocalDateTime) : Boolean {
+//        return a==b
+//    }
+//}
+//
+//fun main() {
+//    println(DatetimeUtils.now)
+//    println(DatetimeUtils.now)
+//    println(DatetimeUtils.now)
+//
+//    println(DatetimeUtils.DEFAULT_FORMAT)
+//
+//    val now = LocalDateTime.now()
+//    println(DatetimeUtils.same(now, now))
+//
+//}
+
+class MyClass {
+
+    private constructor()
+
+    //동반객체
+    companion object MyCompanion {
+        val a = 1234
+
+        fun newInstance() = MyClass()
+
+    }
+}
+
+fun main() {
+    println(MyClass.a)
+    println(MyClass.newInstance())
+
+//    println(MyClass.MyCompanion.a)
+//    println(MyClass.MyCompanion.newInstance())
+}
